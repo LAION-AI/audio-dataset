c22e3ec0230210bd25008771eb92e125173974f8
lukewys <wuyusongwys@gmail.com>
2022-04-09 00:15:07 -0400

    Update preprocess code

--- download_script/download_and_preprocess.sh
+++ download_script/download_and_preprocess.sh
@@ -7,3 +7,9 @@ mkdir raw_datasets
 wget -r -np -nH --cut-dirs=1 -R index.html https://deploy.laion.ai/0fed69941baaabaeccedc2aaaaaaaaaa/Clotho/
 wget -r -np -nH --cut-dirs=1 -R index.html https://deploy.laion.ai/0fed69941baaabaeccedc2aaaaaaaaab/audiocaps/
 wget -r -np -nH --cut-dirs=1 -R index.html https://deploy.laion.ai/0fed69941baaabaeccedc2aaaaaaaaab/BBCSoundEffectsComplete/
+
+cd /mnt/audio_clip/audio-dataset
+conda activate audio_clip
+python ./data_preprocess/preprocess_audiocaps.py
+python ./data_preprocess/preprocess_bbc.py
+python ./data_preprocess/preprocess_clotho.py
