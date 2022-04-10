3bc3450b943c2ae688d8edc95d0eb295846f6ef7
Tianyu Zhang <53099276+tianyu-z@users.noreply.github.com>
2022-04-09 22:19:00 -0400

    Update download_and_preprocess.sh

--- download_script/download_and_preprocess.sh
+++ download_script/download_and_preprocess.sh
@@ -14,3 +14,7 @@ conda activate audio_clip
 python ./data_preprocess/preprocess_audiocaps.py
 python ./data_preprocess/preprocess_bbc.py
 python ./data_preprocess/preprocess_clotho.py
+
+python ./utils/make_tar.py --input /mnt/audio_clip/processed_datasets/audiocaps/ --output /mnt/audio_clip/webdataset_tar/audiocaps/ --dataclass all
+python ./utils/make_tar.py --input /mnt/audio_clip/processed_datasets/BBCSoundEffects/ --output /mnt/audio_clip/webdataset_tar/BBCSoundEffects/ --dataclass all
+python ./utils/make_tar.py --input /mnt/audio_clip/processed_datasets/Clotho/ --output /mnt/audio_clip/webdataset_tar/Clotho/ --dataclass all
