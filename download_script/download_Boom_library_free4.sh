cb9555066af1aabb683eb2ab866254b7fa1cb5da
YuchenHui22314 <yuchen22314@gmail.com>
2022-07-18 15:35:19 -0400

    Add vocalSketch and vocal imitation

--- download_script/download_Boom_library_free4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-wget -c  https://boomlibrary.akamaized.net/boomlibrary/down/zu-23-2_anti-aircraft_gun_153mb.zip?download_file=270365&order=wc_order_Bmz2j72K58sor&uid=a6c562e57d8957d3bdd935fcb31c942a43bd5d6a72c2a199391226ba292212e1&key=814e176169bb729f30d46972f6d12ea5
-
-wget -c https://boomlibrary.akamaized.net/boomlibrary/down/fsdw_520mb.zip?download_file=230959&order=wc_order_Bmz2j72K58sor&uid=a6c562e57d8957d3bdd935fcb31c942a43bd5d6a72c2a199391226ba292212e1&key=814e176169bb729f30d46972f6d12ea5
-
-wget -c https://boomlibrary.akamaized.net/boomlibrary/down/fscs_264mb.zip?download_file=5183&order=wc_order_Bmz2j72K58sor&uid=a6c562e57d8957d3bdd935fcb31c942a43bd5d6a72c2a199391226ba292212e1&key=814e176169bb729f30d46972f6d12ea5
-
-wget -c https://boomlibrary.akamaized.net/boomlibrary/down/fsfl_60mb.zip?download_file=5186&order=wc_order_Bmz2j72K58sor&uid=a6c562e57d8957d3bdd935fcb31c942a43bd5d6a72c2a199391226ba292212e1&key=0ce74e623e86cbf8de435aabd4096dd3
-
