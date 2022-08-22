bc1fbad569cb9526e605b7f2bb1d0326f002462f
knoriy <knoriy@ip-172-31-36-138.ec2.internal>
2022-08-22 11:21:03 +0000

    Improved multiprocessing workload, processing improve from 3 it/s to 30000+ it/s

--- current_dataset/preprocess_mswc.py
+++ current_dataset/preprocess_mswc.py
@@ -14,13 +14,19 @@ import json
 import shutil
 import fsspec
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
+import multiprocessing
+from multiprocessing import Pool
+from itertools import repeat
+
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from utils.audio_utils import audio_to_flac
 from utils.make_tar_utils import tardir
 
-def convert_and_json_dump(file:str, dest:str, df, overwrite:bool=False):
+def convert_and_json_dump(df:pd.DataFrame, overwrite:bool=False, verbose:bool=False):
+    dest = df['dest_path']
+    file = df['src_path']
+
     if os.path.isfile(dest) and overwrite==False:
         print(f'{dest} already exists, skiping')
         return
@@ -28,23 +34,16 @@ def convert_and_json_dump(file:str, dest:str, df, overwrite:bool=False):
     with open(dest.replace('.flac', '.json'), 'w') as f:
         json.dump({'filename': os.path.join(*dest.split('/')[4:]), 'text':[df['WORD']], 'original_data':{'gender':df['GENDER'], 'language':dest.split('/')[-2]}}, f)
 
-
-def split_all_audio_files(df, src_root_path, dest_root_path, max_workers=96):
-    if not os.path.exists(dest_root_path):
-        raise FileNotFoundError(f'Please Check {dest_root_path} exists')
-
-    l = len(df)
-    with tqdm.tqdm(total=l, desc=f'Processing {dest_root_path}') as pbar:
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            threads = [executor.submit(convert_and_json_dump, os.path.join(src_root_path, row['LINK']), os.path.join(dest_root_path, f'{i}.flac'), row) for i, row in enumerate(df.iloc())]
-            for _ in as_completed(threads):
-                pbar.update(1)
+def split_all_audio_files(df, overwrite:bool=False, verbose:bool=False, chunksize:int=1):
+    print(f'starting pool')
+    with Pool() as pool:
+        for result in tqdm.tqdm(pool.starmap(convert_and_json_dump, zip(df.iloc, repeat(overwrite), repeat(verbose)), chunksize=chunksize), total=len(df)):
+            pass
 
 if __name__ == '__main__':
-    import multiprocessing
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--job", required=True)
+    parser.add_argument("--job", help='Directory to the files to process, e.g. "/home/knoriy/fsx/raw_datasets/mswc/audio/fr.tar.gz" ', required=True)
     args = parser.parse_args()
     
     max_workers = multiprocessing.cpu_count()
@@ -64,22 +63,14 @@ if __name__ == '__main__':
 
     dir = args.job
 
-    if dir == '/home/knoriy/fsx/raw_datasets/mswc/audio/en.tar.gz':
-        audio_path = dir
-        audio_path = os.path.split(audio_path)[0]
+    with tarfile.open(dir, mode='r:gz') as mswc_audio:
+        audio_path = os.path.split(dir)[0]
+        mswc_audio.extractall(audio_path)
 
-        splits_path = dir.replace('audio', 'splits')
+    splits_path = dir.replace('audio', 'splits')
+    with tarfile.open(splits_path, mode='r:gz') as mswc_split:
         splits_path = splits_path.replace('.tar.gz', '/')
-    else:
-        audio_path = dir
-        with tarfile.open(audio_path, mode='r:gz') as mswc_audio:
-            audio_path = os.path.split(audio_path)[0]
-            mswc_audio.extractall(audio_path)
-
-        splits_path = dir.replace('audio', 'splits')
-        with tarfile.open(splits_path, mode='r:gz') as mswc_split:
-            splits_path = splits_path.replace('.tar.gz', '/')
-            mswc_split.extractall(splits_path)
+        mswc_split.extractall(splits_path)
 
     tmp = glob.glob(os.path.join(splits_path, '**.csv'), recursive=True)
     csv_paths = []
@@ -96,8 +87,6 @@ if __name__ == '__main__':
             train_test_dev = 'valid/'
         else:
             train_test_dev = 'other/'
-        df = pd.read_csv(csv_path)
-
         # Convert to .flac
         dest_path  = splits_path.replace('.tar.gz', '/').replace('/raw_datasets/', '/processed_datasets/').replace('splits/', '')
         dest_path  = os.path.join(dest_path, train_test_dev)
@@ -106,15 +95,19 @@ if __name__ == '__main__':
         os.makedirs(dest_path, exist_ok=True)
         os.makedirs(src_path, exist_ok=True)
 
+        df = pd.read_csv(csv_path)
+        df['dest_path'] = [os.path.join(dest_path, f'{i}.flac') for i, _ in enumerate(df.iloc())]
+        df['src_path'] = [os.path.join(src_path, row['LINK']) for i, row in enumerate(df.iloc())]
+
         split_all_audio_files(df, src_path, dest_path, max_workers)
 
-        tardir(dest_path, dest_path, chunk, delete_file=True)
+    #     tardir(dest_path, dest_path, chunk, delete_file=True)
 
-        # upload to s3 and delete local
-        s3.put(dest_path, os.path.join(s3_dest, os.path.basename(dir.split('.')[0]), train_test_dev), recursive=True)
-        print('File Uploaded to: ', os.path.join(s3_dest, os.path.basename(dir.split('.')[0]), train_test_dev))
-        shutil.rmtree(dest_path)
+    #     # upload to s3 and delete local
+    #     s3.put(dest_path, os.path.join(s3_dest, os.path.basename(dir.split('.')[0]), train_test_dev), recursive=True)
+    #     print('File Uploaded to: ', os.path.join(s3_dest, os.path.basename(dir.split('.')[0]), train_test_dev))
+    #     shutil.rmtree(dest_path)
 
-    # clean extracted files
-    shutil.rmtree(splits_path.replace('splits/', 'audio/'))
-    shutil.rmtree(splits_path)
+    # # clean extracted files
+    # shutil.rmtree(splits_path.replace('splits/', 'audio/'))
+    # shutil.rmtree(splits_path)
