51525cf36d98cd16e443b10d3be53bf8bc5f5a64
krishna <krishnakalyan3@gmail.com>
2022-09-03 10:38:38 +0200

    download script

---
+++ download_script/download_TAU_MACS.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in {1..21}
+do
+    AUDIO_URL="https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.${i}.zip"
+    wget -c $AUDIO_URL
+done
+
+wget -c https://zenodo.org/record/5114771/files/MACS.yaml
+wget -c https://zenodo.org/record/5114771/files/MACS_competence.csv?download=1
