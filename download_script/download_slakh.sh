435142dfad98d48b6fd10ab005b5c6b34fdffebf
krishna <krishnakalyan3@gmail.com>
2022-10-17 12:39:22 +0200

    fix download

--- download_script/download_slakh.sh
+++ download_script/download_slakh.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-wget "https://zenodo.org/record/4599666/files/slakh2100_flac_redux.tar.gz?download=1" -c -O 'slakh2100_flac_redux.tar1.gz' || exit
+wget https://zenodo.org/record/4599666/files/slakh2100_flac_redux.tar.gz?download=1
+tar -xvzf slakh2100_flac_redux.tar.gz
+rm -rf slakh2100_flac_redux.tar.gz
