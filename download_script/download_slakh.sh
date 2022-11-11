e3cc761def211c26784ffbcfad98624accd15bb9
krishna kalyan <krishna@ip-172-31-38-204.ec2.internal>
2022-11-11 14:14:32 +0000

    slakh process

--- download_script/download_slakh.sh
+++ download_script/download_slakh.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-wget https://zenodo.org/record/4599666/files/slakh2100_flac_redux.tar.gz?download=1
+# cd /fsx/home-krishna/slakh
+
+wget "https://zenodo.org/record/4599666/files/slakh2100_flac_redux.tar.gz?download=1" -c -O 'slakh2100_flac_redux.tar.gz' 
 tar -xvzf slakh2100_flac_redux.tar.gz
 rm -rf slakh2100_flac_redux.tar.gz
