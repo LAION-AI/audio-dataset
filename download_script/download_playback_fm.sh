a078e506ca33949be3c7dd899aae95da8c1de917
krishna kalyan <krishna@ip-172-31-44-155.ec2.internal>
2023-02-06 13:20:55 +0000

    WIP download script

---
+++ download_script/download_playback_fm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# cd /fsx/home-krishna/playback_fm
+
+wget "https://github.com/marianna13/audio-dataset/blob/main/raw_datasets/PLAYBACK_FM/PLAYBACK_FM.parquet?raw=true" -c -O 'playback_fm.parquet'
